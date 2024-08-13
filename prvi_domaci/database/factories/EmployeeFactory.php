baf5b3a2bbe7a07b1fce769e62d6d38bd44c642b
Nikola Pavic <np20211046@student.fon.bg.ac.rs>
2024-08-13 13:14:39 +0200

    Kreirani factory i seeder za sve modele.

---
+++ prvi_domaci/database/factories/EmployeeFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Company;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employee>
+ */
+class EmployeeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'company_id' => Company::factory(),
+            'name' => $this->faker->name,
+            'position' => $this->faker->jobTitle,
+        ];
+    }
+}
--- prvi_domaci/database/seeders/DatabaseSeeder.php
+++ prvi_domaci/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
+use App\Models\Employee;
 use App\Models\User;
+use App\Models\File;
+use App\Models\Privilege;
+use Illuminate\Container\Attributes\DB;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +20,49 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+       
+        // Privilege::truncate();
+        // File::truncate();
+        // Employee::truncate();
+        // Company::truncate();
+        // User::truncate();
+       
+
+
+
+
+        // $user1 = User::factory()->create();
+        // $user2 = User::factory()->create();
+        // $company1= Company::factory()->create([
+        //     'user_id'=>$user1->id
+        // ]);
+        // $company2= Company::factory()->create([
+        //     'user_id'=>$user2->id
+        // ]);
+
+        $user1 = User::factory()->create();
+        $user2 = User::factory()->create();
+        $company1 = Company::factory()->create(['user_id'=>$user1->id]);
+        $company2 = Company::factory()->create(['user_id'=>$user2->id]);
+         Employee::factory()->count(5)->create(['company_id' => $company1->id]);
+        Employee::factory()->count(5)->create(['company_id' => $company2->id]);
+        $file = File::factory()->count(5)->create(['user_id' => $user1->id]);
+        $privilege = Privilege::factory()->count(5)->create();
+
+
+        // Employee::factory(4)->create([
+        //     'company_id'=>$company1->id
+        // ]);
+        // Employee::factory(4)->create([
+        //     'company_id'=>$company2->id
+        // ]);
+        // File::factory(10)->create();
+        // Privilege::factory(20)->create();
+
+
     }
 }
