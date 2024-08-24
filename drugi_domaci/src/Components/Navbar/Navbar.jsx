ae1e57f785f4768e6df88c9cd48893322cac58f9
Bozidar Kapetanov <bozidar.kapetanov@fonis.rs>
2024-08-24 11:23:27 +0200

    kreirana komponenta navbar

---
+++ drugi_domaci/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import "./Navbar.css";
+import React from "react";
+import search_w from "../Assets/search-w.png";
+import logo_b from "../Assets/logo-black.png";
+import { Outlet } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div>
+      <div className="navbar">
+        <img src={logo_b} alt="" className="logo" />
+        <ul>
+          <li>Home</li>
+          <li>Employees</li>
+          <li>Files</li>
+          <li>Profile</li>
+        </ul>
+        <div className="search-box">
+          <input type="text" placeholder="Serach" />
+          <img src={search_w} alt="" />
+        </div>
+        <a href="/loginRegister" className="logout">
+          Logout
+        </a>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
+
+export default Navbar;
