55c3cc307ce6586812ae2fbe8ec71b61515412b9
Nikola Pavic <np20211046@student.fon.bg.ac.rs>
2024-09-03 13:41:00 +0200

    Sredjeno rutiranje

--- drugi_domaci/src/App.js
+++ drugi_domaci/src/App.js
@@ -1,7 +1,13 @@
 import { useState, useEffect } from "react";
 import LoginRegister from "./Components/LoginRegister/LoginRegister";
 import Navbar from "./Components/Navbar/Navbar.jsx";
-import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Router,
+  Navigate,
+} from "react-router-dom";
 import "./index.css";
 import Company from "./Components/Company/Company.jsx";
 import Home from "./Components/Home/Home.jsx";
@@ -42,40 +48,83 @@ function App() {
     }
   }, [company]);
 
+  // return (
+  //   <BrowserRouter className="container">
+  //     <Routes>
+  //       <Route path="" element={<Navbar />}>
+  //         <Route
+  //           path="/"
+  //           element={
+  //             <Home
+  //               company={company}
+  //               employeesCount={employeesCount}
+  //               setEmployeesCount={setEmployeesCount}
+  //               user_id={user?.id}
+  //             />
+  //           }
+  //         />
+  //         <Route
+  //           path="/employees"
+  //           element={
+  //             <Employees
+  //               company={company}
+  //               setEmployeesCount={setEmployeesCount}
+  //             />
+  //           }
+  //         />
+  //         <Route path="/files" element={<Files user={user} />} />
+  //       </Route>
+  //       <Route
+  //         path="/loginRegister"
+  //         element={<LoginRegister setUser={setUser} />}
+  //       />
+  //       <Route
+  //         path="/createCompany"
+  //         element={<Company user={user} setCompany={setCompany} />}
+  //       />
+  //     </Routes>
+  //   </BrowserRouter>
+  // );
+
   return (
     <BrowserRouter className="container">
       <Routes>
-        <Route path="" element={<Navbar />}>
-          <Route
-            path="/"
-            element={
-              <Home
-                company={company}
-                employeesCount={employeesCount}
-                setEmployeesCount={setEmployeesCount}
-                user_id={user?.id}
-              />
-            }
-          />
-          <Route
-            path="/employees"
-            element={
-              <Employees
-                company={company}
-                setEmployeesCount={setEmployeesCount}
-              />
-            }
-          />
-          <Route path="/files" element={<Files user={user} />} />
-        </Route>
         <Route
           path="/loginRegister"
           element={<LoginRegister setUser={setUser} />}
         />
-        <Route
-          path="/createCompany"
-          element={<Company user={user} setCompany={setCompany} />}
-        />
+
+        {user ? (
+          <Route path="/" element={<Navbar />}>
+            <Route
+              path="/"
+              element={
+                <Home
+                  company={company}
+                  employeesCount={employeesCount}
+                  setEmployeesCount={setEmployeesCount}
+                  user_id={user?.id}
+                />
+              }
+            />
+            <Route
+              path="/employees"
+              element={
+                <Employees
+                  company={company}
+                  setEmployeesCount={setEmployeesCount}
+                />
+              }
+            />
+            <Route path="/files" element={<Files user={user} />} />
+            <Route
+              path="/createCompany"
+              element={<Company user={user} setCompany={setCompany} />}
+            />
+          </Route>
+        ) : (
+          <Route path="*" element={<Navigate to="/loginRegister" />} />
+        )}
       </Routes>
     </BrowserRouter>
   );
